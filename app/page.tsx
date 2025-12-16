6abdcf176c871fb5498905eec64e42257f733ea0
Pratoosh-18 <pratoosh10garg@gmail.com>
2025-12-17 00:02:52 +0530

    theme setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Header from "./components/Header";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold">Home</h1>
+      </div>
+
+      <div className="container mx-auto">
+        This is a notepad app.
+
+        <input type="text" className="border-2 border-gray-300 rounded-md p-2" placeholder="Enter your note" />
+        <button>Save</button>
+      </div>
+
     </>
   );
 }
